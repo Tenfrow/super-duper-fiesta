2b1a8f85924d6b6644ec1c959ffe753f9ec6afe4
Tenfrow <vit2zremb@gmail.com>
2018-11-03 01:22:13 +0200

    Add string representation for models

--- storage/models.py
+++ storage/models.py
@@ -5,6 +5,9 @@ class Seller(models.Model):
     name = models.CharField(max_length=255)
     birthday = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -12,3 +15,6 @@ class Product(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='products')
     date_added = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return '{} (seller: {})'.format(self.name, self.seller)
