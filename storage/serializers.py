d8e28f00e7c08f4aeacc0727dec85f5890c90f3f
Tenfrow <vit2zremb@gmail.com>
2018-11-03 00:10:54 +0200

    Create API for Product CRUD and Seller list

--- storage/serializers.py
+++ storage/serializers.py
@@ -3,11 +3,17 @@ from rest_framework import serializers
 from .models import Product, Seller
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-
-
 class SellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seller
+        fields = ['id', 'name', 'birthday']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    date_added = serializers.DateField(read_only=True)
+    date_updated = serializers.DateField(read_only=True)
+    seller = serializers.PrimaryKeyRelatedField(queryset=Seller.objects.all())
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'seller', 'date_added', 'date_updated']
--- storage/views.py
+++ storage/views.py
@@ -1,9 +1,19 @@
 from rest_framework import generics
 
-from storage.models import Seller
-from storage.serializers import SellerSerializer
+from storage.models import Seller, Product
+from storage.serializers import SellerSerializer, ProductSerializer
 
 
 class SellerList(generics.ListAPIView):
     queryset = Seller.objects.all()
     serializer_class = SellerSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
