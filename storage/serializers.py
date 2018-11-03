a5a060b72fe54dbbef1bd898984e27207d8bb111
Tenfrow <vit2zremb@gmail.com>
2018-11-03 04:37:50 +0200

    Add Product serializer without seller

--- storage/serializers.py
+++ storage/serializers.py
@@ -3,13 +3,6 @@ from rest_framework import serializers
 from .models import Product, Seller
 
 
-class SellerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Seller
-        fields = ['id', 'name', 'birthday', 'products']
-        depth = 1
-
-
 class ProductSerializer(serializers.ModelSerializer):
     date_added = serializers.DateField(read_only=True)
     date_updated = serializers.DateField(read_only=True)
@@ -19,3 +12,18 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'seller', 'date_added', 'date_updated']
+
+
+class ProductSimpleSerializer(ProductSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'date_added', 'date_updated']
+
+
+class SellerSerializer(serializers.ModelSerializer):
+    products = ProductSimpleSerializer(many=True)
+
+    class Meta:
+        model = Seller
+        fields = ['id', 'name', 'birthday', 'products']
+        depth = 1
